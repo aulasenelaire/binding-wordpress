babfdae679167cda5674fda014f6d7b8ff1738e3
Andres Gutierrez <andres@redbooth.com>
2015-04-27 09:59:35 +0200

    Update Wordpress to 4.2

--- web/app/themes/binding/templates/page-header.php
+++ web/app/themes/binding/templates/page-header.php
@@ -5,7 +5,7 @@
     <?= Titles\title(); ?>
   </h1>
   <?php
-    $text  = get_post_meta( get_the_ID(), '_binding_textarea', true );
+    $text  = get_post_meta(get_the_ID(), '_binding_textarea', true );
     echo $text;
   ?>
 </div>
